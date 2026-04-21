88308435055ed157ee88fbfe21dbbe947b51a9d7
patoune <patrflorentin@gmail.com>
2026-04-21 16:08:55 +0200

    git cleanup

--- components/footer.php
+++
@@ -1,1 +0,0 @@
-<a href="https://www.flaticon.com/free-icons/no-photo" title="no photo icons">No photo icons created by Those Icons - Flaticon</a>
--- components/header.php
+++
@@ -1,9 +0,0 @@
-<header>
-    <h1>Véhicules</h1>
-
-    <nav>
-        <a href="/index.php">Tous</a>
-        <a href="/index.php?type=location">Location</a>
-        <a href="/index.php?type=vente">Achat</a>
-    </nav>
-</header>
