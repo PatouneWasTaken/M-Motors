3a224b0415da00a2624fbc812ceba36f1bac164e
patoune <patrflorentin@gmail.com>
2026-06-15 17:29:10 +0200

    bug fixes 2 dashboard

--- views/admin/vehicles-V.php
+++ views/admin/vehicles-V.php
@@ -1,24 +1,20 @@
-<section>
+<?php require_once __DIR__ . "/../../toolbox/tools.php"; ?>
+
+<section class="admin-vehicles">
 
 <?php foreach ($vehicles as $v): ?>
 
-	<article>
+	<article class="admin-vehicle-row">
 
-    	<p>
-			<?= e($v['name']) ?>
-		</p>
+    	<p class="name"><?= e($v['brand'] . ' ' . $v['model']) ?></p>
 
-    	<p>
-			<?= e($v['brand']) ?>
-		</p>
+    	<p class="type"><?= e(vehicleType($v['type'])) ?></p>
 
-    	<p>
-			<?= e($v['type']) ?>
-		</p>
+    	<p class="price"><?= number_format($v['price'], 0, ',', ' ') ?> €</p>
 
-    	<p>
-        	<a href="/index.php?page=admin_edit_vehicle&id=<?= $v['id'] ?>">Edit</a>
-        	<a href="/index.php?page=admin_delete_vehicle&id=<?= $v['id'] ?>">Delete</a>
+    	<p class="actions">
+        	<a href="/M-Motors/public/index.php?page=admin_edit_vehicle&id=<?= (int)$v['id'] ?>">Modifier</a>
+        	<a href="/M-Motors/public/index.php?page=admin_delete_vehicle&id=<?= (int)$v['id'] ?>">Supprimer</a>
 		</p>
 
 	</article>
