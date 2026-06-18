dbb6113e44ce0836b0c7d3a502a1b3c90bebccb6
patoune <patrflorentin@gmail.com>
2026-06-18 19:00:38 +0200

    ajout responsive

--- views/components/head.php
+++ views/components/head.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?= 'M-Motors' . ($title ? ' - ' . $title : '') ?></title>
 	<link rel="icon" href="/M-Motors/public/assets/letter-m.png">
     <link rel="stylesheet" href="/M-Motors/public/styles/style.css">
